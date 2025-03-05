bd4ea759b3e3a6e42a5ee0384ba2089f7a9dd871
System <dimasoft976@gmail.com>
2025-03-05 23:22:06 +0300

    A file capable of executing a file (or files)

---
+++ main.py
@@ -0,0 +1,16 @@
+import sys
+from lexer import *
+from parser import *
+
+if __name__ == '__main__':
+    if len(sys.argv) == 1:
+        sys.exit(1)
+    try:
+        poss = 1
+        while poss < len(sys.argv):
+            with open(sys.argv[poss], 'r', encoding='utf-8') as f:
+                code = f.read()
+                parse(lex(code))
+            poss += 1
+    except:
+        sys.exit(1)
